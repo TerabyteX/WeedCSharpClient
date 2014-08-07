f31db34f71333d2f6ad714fb56aba30796e863db
TerabyteX <don_knuth@163.com>
2014-08-08 01:38:02 +0800

    achieve replication type

--- WeedCSharpClient/ReplicationStrategy.cs
+++ WeedCSharpClient/ReplicationStrategy.cs
@@ -4,7 +4,7 @@
     public enum ReplicationStrategy
     {
         /// <summary>
-        /// no replication
+        /// no replication, just one copy
         /// </summary>
         None = 0,
         /// <summary>
@@ -12,20 +12,32 @@
         /// </summary>
         OnceOnSameRack = 1,
         /// <summary>
-        /// replicate once on a different rack, but same data center
+        /// replicate twice on the same rack
+        /// </summary>
+        TwiceOnSameRack = 2,
+        /// <summary>
+        /// replicate once on a different rack in the same data center
         /// </summary>
         OnceOnDifferentRack = 10,
+        /// <summary>
+        /// replicate twice on a different rack in the same data center
+        /// </summary>
+        TwiceOnDifferentRack = 20,
         /// <summary>
         /// replicate once on a different data center
         /// </summary>
         OnceOnDifferentDC = 100,
         /// <summary>
-        /// replicate twice on two different data center
+        /// replicate twice on two other different data center
         /// </summary>
         TwiceOnDifferentDC = 200,
         /// <summary>
-        /// replicate once on a different rack, and once on a different data center
+        /// replicate once on a different rack, once on a different data center
         /// </summary>
-        OnceOnDifferentRackAndOnceOnDifferentDC = 110
+        OnceOnDifferentRackAndOnceOnDifferentDC = 110,
+        /// <summary>
+        /// replicate once on the same rack, once on a different rack, once on a different data center
+        /// </summary>
+        OnceOnSameRackAndOnceOnDifferentRackAndOnceOnDifferentDC = 111
     }
 }
