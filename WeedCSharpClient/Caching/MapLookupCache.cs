32c6abf42622fcbe38662caeb723f00e8f02d045
TerabyteX <Don_Knuth@163.com>
2014-10-06 22:56:14 +0800

    cache number

--- WeedCSharpClient/Caching/MapLookupCache.cs
+++ WeedCSharpClient/Caching/MapLookupCache.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 
 namespace WeedCSharpClient.Caching
 {
     public class MapLookupCache : ILookupCache
     {
         private ConcurrentDictionary<long, List<Location>> _cache = new ConcurrentDictionary<long, List<Location>>();
+        private readonly int _cacheNum;
+
+        public MapLookupCache()
+        {
+            var cacheNum = ConfigurationManager.AppSettings["CacheNum"];
+
+            _cacheNum = cacheNum != null ?
+                int.Parse(cacheNum) : 10000;
+        }
 
         #region Implement ILookupCache
 
@@ -29,6 +40,12 @@ namespace WeedCSharpClient.Caching
 
         public void SetLocation(long volumeId, List<Location> locations)
         {
+            if (_cache.Count() > _cacheNum)
+            {
+                var id = _cache.First().Key;
+                Invalidate(id);
+            }
+
             _cache.TryAdd(volumeId, locations);
         }
 
